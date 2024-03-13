9938f2dca8dc1e1c3f16f1189294abb73a732266
Albany Timbó Mesquita <4lb4ny@gmail.com>
2024-03-13 16:28:41 -0300

    Final comit

---
+++ Traducao/Assets/Tradutor/Scripts/MenuManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using DFTGames.Localization;
+
+namespace DFTGames
+{
+    public class MenuManager : MonoBehaviour
+    {
+        #region Public Methods
+
+        public void SetEnglish()
+        {
+            Localize.SetCurrentLanguage(SystemLanguage.English);
+            LocalizeImage.SetCurrentLanguage(SystemLanguage.English);
+        }
+        public void SetPortuguese()
+        {
+            Localize.SetCurrentLanguage(SystemLanguage.Portuguese);
+            LocalizeImage.SetCurrentLanguage(SystemLanguage.Portuguese);
+        }
+        public void SetSpanish()
+        {
+            Localize.SetCurrentLanguage(SystemLanguage.Spanish);
+            LocalizeImage.SetCurrentLanguage(SystemLanguage.Spanish);
+        }
+        
+
+        #endregion Public Methods
+    }
+}
